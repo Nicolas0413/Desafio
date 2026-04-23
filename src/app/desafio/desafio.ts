16525aae1b58c93d69a436aa629b9581c7d27547
BernardoDev001 <luzdasilvabernardo@gmail.com>
2026-04-23 15:13:11 -0300

    Inicio do desafio

---
+++ src/app/desafio/desafio.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-desafio',
+  templateUrl: 'desafio.html',
+  styleUrls: ['desafio.scss'],
+})
+
+
+export class desafioComponent {
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { desafioComponent } from '../desafio/desafio';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [desafioComponent ,IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
   constructor() {}
